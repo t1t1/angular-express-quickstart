f3f3586c59403d1a79dc376c40357b1bdedbe0ff
t1t1 <cha@hanmail2.net>
2016-07-09 11:53:46 +0900

    supertest.js;

--- routes/projects.spec.js
+++ routes/projects.spec.js
@@ -1,11 +1,13 @@
+var app = require("../app.js");
+var supertest = require("supertest");
+var request = supertest(app);
+
 describe('Projects Test', () => {
-    
     describe('GET /projects', () => {
-        
         it('shoud return project list', function(done) {
-            done();
+            request.get("/projects").expect(200, {
+                data: [{id:1, name: "work2"}]
+            }, done); // move done()
         });
-        
     });
-    
 });
