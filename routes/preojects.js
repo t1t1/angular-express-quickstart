e5d60cfe0b6f0fb2c934ecb419da834eec0b0d41
t1t1 <cha@hanmail2.net>
2016-07-09 11:38:11 +0900

    add projects.js

---
+++ routes/preojects.js
@@ -0,0 +1,19 @@
+var express = require("express"),
+    router = express.Router(); //
+    
+var data = {
+    data: [
+        {id: 1, name: "work"}
+    ]
+};
+
+router.get("/", function(req, res) {
+    res.json(data)
+});
+
+router.get("/:projectId", function(req, res) {
+    res.json(data.data[0]); // TODO
+});
+
+    
+module.exports = router;
